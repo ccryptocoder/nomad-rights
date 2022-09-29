64f369bbbd381b8e2d089ca243ec6dd69cd680c4
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-09-29 13:31:18 +0600

    Create news section

---
+++ app/assets/js/news.js
@@ -0,0 +1,17 @@
+const newsEvents = ['resize', 'DOMContentLoaded'];
+
+projEvents.forEach(e => {
+    window.addEventListener(e, () => {
+        if (window.matchMedia("(max-width: 1023px)").matches) {
+            const newsSwiper = new Swiper('#newsSwiper', {
+                loop: false,
+              
+                // Navigation arrows
+                navigation: {
+                    prevEl: '#newsPrev',
+                    nextEl: '#newsNext',
+                },
+            });
+        }
+    })
+})
