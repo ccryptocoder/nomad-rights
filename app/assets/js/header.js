ded61c7a663e670e6de7f74f48853368a3cda12c
Emiliya <81705094+ccryptocoder@users.noreply.github.com>
2022-10-18 18:59:48 +0600

    Minor adjustments

--- app/assets/js/header.js
+++ app/assets/js/header.js
@@ -1,23 +1,21 @@
-// window.addEventListener('DOMContentLoaded', () => {
-    
-// })
+window.addEventListener('DOMContentLoaded', () => {
+    const headerMid = document.querySelector('[data-header-mid]');
+    const headerBtn = document.querySelector('[data-header-btn]');
 
-const headerMid = document.querySelector('[data-header-mid]');
-const headerBtn = document.querySelector('[data-header-btn]');
+    headerBtn.addEventListener('click', () => {
+        headerBtn.classList.toggle('active');
+        headerMid.classList.toggle('active');
+    });
 
-headerBtn.addEventListener('click', () => {
-    headerBtn.classList.toggle('active');
-    headerMid.classList.toggle('active');
-});
+    const headerSearchBtn = document.querySelector('[data-header-search-btn]');
+    const headerSearch = document.querySelector('[data-header-search]');
 
-const headerSearchBtn = document.querySelector('[data-header-search-btn]');
-const headerSearch = document.querySelector('[data-header-search]');
+    headerSearchBtn.addEventListener('click', () => {
+        headerSearch.classList.toggle('active');
+    })
 
-headerSearchBtn.addEventListener('click', () => {
-    headerSearch.classList.toggle('active');
+    const base = document.createElement('base');
+    base.href = 'https://ccryptocoder.github.io/nomad-rights/app/'; // /nomad-rights/app/
+
+    document.head.appendChild(base);
 })
-
-const base = document.createElement('base');
-base.href = 'https://ccryptocoder.github.io/nomad-rights/app/'; // /nomad-rights/app/
-
-document.head.appendChild(base);
